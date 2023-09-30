bb11b176007762e4faf52b4fe99a4e904b7e156f
rmf2pt <rmf2pt@gmail.com>
2023-09-30 19:14:28 +0100

    Initial commit for remote repo
    Signed-off-by: rmf2pt <rmf2pt@gmail.com>

---
+++ __tests__/game.test.js
@@ -0,0 +1,191 @@
+/**
+ * @vitest-environment happy-dom
+ */
+import { vi, expect, test } from "vitest";
+import {
+  updateCompletedRows,
+  getCompletedRows,
+  calculateScore,
+  updateScore,
+  getScoreValue,
+  setScoreValue,
+  updateLevel,
+  getLevel,
+  setLevel,
+  setCompletedRows,
+  setGameSpeed,
+  getGameSpeed,
+} from "../tetris/src/game";
+import { beforeEach } from "vitest";
+
+beforeEach(async () => {
+  await setLevel(1);
+  await setScoreValue(0);
+});
+
+// Verify that tests is working correctly
+test("testing tests", () => {
+  expect(Math.sqrt(4)).toBe(2);
+});
+
+// Test case: Testing the updateCompletedRows function
+test("Update completedRows with positive value", () => {
+  // Call the function to update completedRows with a positive value (e.g., 3)
+  updateCompletedRows(3);
+
+  // Assert that completedRows has been updated correctly
+  expect(getCompletedRows()).toBe(3);
+});
+
+// Test case: Testing the updateCompletedRows function with zero
+test("Update completedRows with zero value", () => {
+  // Call the function to update completedRows with zero
+  updateCompletedRows(0);
+
+  // Assert that completedRows remains unchanged (should still be 0)
+  expect(getCompletedRows()).toBe(3);
+});
+
+// Test case: Testing the updateCompletedRows function with negative value
+test("Update completedRows with negative value", () => {
+  // Call the function to update completedRows with a negative value (e.g., -2)
+  updateCompletedRows(-2);
+
+  // Assert that completedRows remains unchanged (should still be 0)
+  expect(getCompletedRows()).toBe(0);
+});
+
+// Test case: Testing single line clear
+test("Calculate score for single line clear", () => {
+  // Simulating one full line clear
+  const fullLines = [1];
+  calculateScore(fullLines);
+  expect(getScoreValue()).toBe(200);
+});
+
+// Test case: Testing double line clear
+test("Calculate score for double line clear", () => {
+  // Simulating two full lines cleared
+  const fullLines = [1, 2];
+  calculateScore(fullLines);
+  expect(getScoreValue()).toBe(400);
+});
+
+// Test case: Testing triple line clear
+test("Calculate score for triple line clear", () => {
+  // Simulating three full lines cleared
+  const fullLines = [1, 2, 3];
+  calculateScore(fullLines);
+  expect(getScoreValue()).toBe(600);
+});
+
+// Test case: Testing tetris line clear
+test("Calculate score for tetris line clear", () => {
+  // Simulating four full lines cleared
+  const fullLines = [1, 2, 3, 4];
+  calculateScore(fullLines);
+  expect(getScoreValue()).toBe(900);
+});
+
+// Test case: Testing no full line
+test("Calculate score for no line clear", () => {
+  // Simulating four full lines cleared
+  const fullLines = [];
+  calculateScore(fullLines);
+  expect(getScoreValue()).toBe(0);
+});
+
+// Test case: Testing level update when 10 full lines are completed
+test("Update level and completed rows for 10 full lines", () => {
+  // Set the initial level and completed rows
+  setLevel(1);
+  setCompletedRows(0);
+
+  // Call the updateLevel function with 10 full lines completed
+  updateLevel(10);
+
+  // Assert that the level has been updated to 2
+  expect(getLevel()).toBe(2);
+
+  // Assert that the completed rows have been updated to 0 (since we completed 10)
+  expect(getCompletedRows()).toBe(0);
+});
+
+// Test case: Testing level update when more than 10 full lines are completed
+test("Update level and completed rows for more than 10 full lines", () => {
+  // Set the initial level and completed rows
+  setLevel(2);
+  setCompletedRows(3); // 3 completed rows from previous level
+
+  // Call the updateLevel function with 15 full lines completed
+  updateLevel(15);
+
+  // Assert that the level has been updated to 3
+  expect(getLevel()).toBe(3);
+
+  // Assert that the completed rows have been updated to 5 (since we completed 15)
+  expect(getCompletedRows()).toBe(5);
+});
+
+// Test case: Testing level update when less than 10 full lines are completed
+test("No level update for less than 10 full lines", () => {
+  // Set the initial level and completed rows
+  setLevel(3);
+  setCompletedRows(7); // 7 completed rows from previous level
+
+  // Call the updateLevel function with 7 full lines completed (less than 10)
+  updateLevel(7);
+
+  // Assert that the level remains the same (level 3)
+  expect(getLevel()).toBe(3);
+
+  // Assert that the completed rows have been updated to 4 (since we completed 7)
+  expect(getCompletedRows()).toBe(7);
+});
+
+// Mocking DOM elements
+const scoreEl = {
+  textContent: null,
+};
+
+// Mocking initial game speed
+const initialGameSpeed = 500;
+
+// Mocking level element (if needed)
+const levelEl = {
+  textContent: null,
+};
+
+// Test case: Testing score update with level increase
+test("Update score, level, and game speed when completing 10 full lines", () => {
+  // Set initial level, completed rows, and game speed
+  setLevel(1);
+  setGameSpeed(500);
+  setCompletedRows(10);
+
+  // Call updateScore with 10 full lines completed
+  updateScore();
+
+  // Assert that the level is increased to 2
+  expect(getLevel()).toBe(2);
+
+  // Assert that the game speed is updated based on the new level
+  expect(getGameSpeed()).toBe(460); // Calculated based on initialGameSpeed - level * 20
+});
+
+// Test case: Testing score update without level increase
+test("Update score and game speed without level increase", () => {
+  // Set initial level, completed rows, and game speed
+  setLevel(4);
+  setCompletedRows(9); // 2 completed rows from previous level
+  setGameSpeed(420);
+
+  // Call updateScore with 3 full lines completed (less than 10)
+  updateScore();
+
+  // Assert that the level remains the same (level 4)
+  expect(getLevel()).toBe(4);
+
+  // Assert that the game speed is updated based on the new level
+  expect(getGameSpeed()).toBe(420); // Calculated based on initialGameSpeed - level * 20
+});
---
+++ tetris/src/game.js
@@ -0,0 +1,249 @@
+import { moveDown, checkColision } from "./inputHandler.js";
+import {
+  piece,
+  resetGrid,
+  clearPiece,
+  clearAllPieces,
+  createFirstPiece,
+  createNextPiece,
+} from "./renderer.js";
+
+const scoreEl = document.getElementById("score");
+const highscoreEl = document.getElementById("high-score");
+const levelEl = document.getElementById("level");
+const startButton = document.getElementById("start-button");
+const messageContainer = document.getElementById("message-container");
+const retryButton = document.getElementById("retry-button");
+const backgroundMusic = document.getElementById("background-music");
+const gameOverMusic = document.getElementById("game-over-music");
+
+const initialGameSpeed = 500;
+
+let scoreValue = 0;
+let level = 1;
+let completedRows = 0;
+let gameSpeed = initialGameSpeed;
+let gameInterval;
+let isGameOver = false;
+
+// TODO Create Unit Tests
+// TODO increase game music with speed
+// TODO make the start button a pause button during in-game
+// TODO make a button to quit game in Game Over screen
+// TODO put a sound when pieces land
+// TODO show a pop-up with the score of the current full lines
+
+function calculateScore(fullLines) {
+  /* 
+  Score Grid:
+  Single Line Clear: 100 points
+  Double Line Clear: 300 points
+  Triple Line Clear: 500 points
+  Tetris Line Clear: 800 points
+  Level Bonus: 100 points per level
+  TODO: implement Soft and Hard Drop
+  Soft Drop Score: 1 point per cell moved downward
+  Hard Drop Score: 2 points per cell dropped
+  */
+  const currentLevel = getLevel();
+  switch (fullLines.length) {
+    case 1:
+      setScoreValue(getScoreValue() + 100 + 100 * currentLevel);
+      break;
+    case 2:
+      setScoreValue(getScoreValue() + 300 + 100 * currentLevel);
+      break;
+    case 3:
+      setScoreValue(getScoreValue() + 500 + 100 * currentLevel);
+      break;
+    case 4:
+      setScoreValue(getScoreValue() + 800 + 100 * currentLevel);
+      break;
+    default:
+      break;
+  }
+}
+
+function getScoreValue() {
+  return scoreValue;
+}
+
+function setScoreValue(newScore) {
+  scoreValue = newScore;
+}
+
+function getLevel() {
+  return level;
+}
+
+function setLevel(newLevel) {
+  level = newLevel;
+}
+
+function getCompletedRows() {
+  return completedRows;
+}
+
+function setCompletedRows(newCompletedRows) {
+  completedRows = newCompletedRows;
+}
+
+function getGameSpeed() {
+  return gameSpeed;
+}
+
+function setGameSpeed(newGameSpeed) {
+  gameSpeed = newGameSpeed;
+}
+
+function updateCompletedRows(fullLines) {
+  if (fullLines >= 0) {
+    setCompletedRows(getCompletedRows() + fullLines);
+  } else {
+    setCompletedRows(0);
+  }
+}
+
+function updateLevel(fullLinesCount) {
+  // Level increases by 1 when 10 full lines are completed
+  if (fullLinesCount >= 10) {
+    setLevel(getLevel() + 1);
+    if (levelEl) {
+      levelEl.textContent = getLevel();
+    }
+    // Full lines above 10 count to next level countdown
+    setCompletedRows(fullLinesCount % 10);
+  }
+}
+
+function updateScore() {
+  // Update the displayed score
+  if (scoreEl) {
+    scoreEl.textContent = getScoreValue();
+  }
+  updateLevel(getCompletedRows());
+  // Update game interval delay based on the level
+  // Higher level = lower game interval delay
+  // Minimum game interval delay = 100
+  setGameSpeed(Math.max(100, initialGameSpeed - getLevel() * 20));
+}
+
+// TODO Continue test coverage
+
+function gameOver() {
+  // Stops the game
+  isGameOver = true;
+  clearInterval(gameInterval);
+  // Stop the background music (e.g., when the game ends)
+  if (backgroundMusic) {
+    backgroundMusic.pause();
+  }
+  // Plays game over music
+  if (gameOverMusic) {
+    gameOverMusic.play();
+  }
+  // Shows message container
+  if (messageContainer) {
+    messageContainer.classList.remove("hidden");
+  }
+  updateHighscore();
+  // Enables start button
+  if (startButton) {
+    startButton.removeAttribute("disabled");
+  }
+}
+
+function updateHighscore() {
+  const finalScore = getScoreValue();
+  if (localStorage.getItem("tetris-highscore")) {
+    const previousHighscore = localStorage.getItem("tetris-highscore");
+    if (finalScore > previousHighscore) {
+      localStorage.setItem("tetris-highscore", finalScore);
+      if (highscoreEl) {
+        highscoreEl.textContent = finalScore;
+      }
+    }
+  } else {
+    localStorage.setItem("tetris-highscore", finalScore);
+  }
+}
+
+function getHighscore() {
+  // Check for highscore on local storage and renders it
+  if (localStorage.getItem("tetris-highscore")) {
+    const previousHighscore = localStorage.getItem("tetris-highscore");
+    if (highscoreEl) {
+      highscoreEl.textContent = previousHighscore;
+    }
+  }
+}
+
+function updateGame() {
+  // Implements the main game loop
+  moveDown(piece);
+  // If the piece coliddes at the first move down, it's game over
+  if (checkColision(piece.shape)) {
+    clearPiece();
+    gameOver();
+    return;
+  }
+  // Schedule the next execution of updateGame with the updated gameSpeed
+  setTimeout(updateGame, gameSpeed);
+}
+
+function startGame() {
+  // Hide the message container
+  if (messageContainer && !messageContainer.classList.contains("hidden")) {
+    messageContainer.classList.add("hidden");
+  }
+  // Start the background music fromthe beginning
+  if (backgroundMusic) {
+    backgroundMusic.currentTime = 0;
+    backgroundMusic.play();
+  }
+  // Initialize game variables and start the game loop
+  isGameOver = false;
+  setScoreValue(0);
+  if (scoreEl) {
+    scoreEl.textContent = 0;
+  }
+  getHighscore();
+  setLevel(1);
+  setGameSpeed(initialGameSpeed);
+  resetGrid();
+  clearAllPieces();
+  createNextPiece();
+  createFirstPiece();
+  updateGame();
+  // Disable the start button so that the user doesn't click ingame
+  if (startButton) {
+    startButton.setAttribute("disabled", "true");
+  }
+}
+
+// Event Listeners
+if (startButton) {
+  startButton.addEventListener("click", startGame);
+}
+if (retryButton) {
+  retryButton.addEventListener("click", startGame);
+}
+
+// On load
+getHighscore();
+
+export {
+  isGameOver,
+  updateCompletedRows,
+  getCompletedRows,
+  setCompletedRows,
+  updateLevel,
+  getLevel,
+  setLevel,
+  calculateScore,
+  getScoreValue,
+  setScoreValue,
+  updateScore,
+  setGameSpeed,
+  getGameSpeed,
+};
